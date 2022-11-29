017610775ddfe1c85e34da952ec5701cb70d6a55
Tejas Bhatt <bhatttejas30@gmail.com>
2022-11-30 01:40:57 +0530

    elite

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Vehicle } from './Vehicle';
-import { Animals } from './Animals';
-import { Fruite } from './Fruite';
+
 import { Piano } from './Piano';
 export const Home1=()=>{
     return (
         <React.Fragment>
-            <Vehicle /><br /><br /><br /><br />
-            <Animals /><br /><br /><br /><br />
-            <Fruite /><br /><br /><br /><br /><br /><br /><br />
+           
             <Piano /><br /><br /><br /><br />
         </React.Fragment>
     )
