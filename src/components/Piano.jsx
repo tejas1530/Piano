ecf189647ddbd65bbf98d43e8952b4edb7fe510b
Tejas Bhatt <bhatttejas30@gmail.com>
2022-11-30 01:34:50 +0530

    fixedbugs

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,8 +1,15 @@
-import React,{useState} from 'react';
+import React,{useRef, useState,useEffect} from 'react';
 import { Button ,Grid,Card,CardContent} from '@mui/material';
 
 export const Piano=()=>{
+    const music=useRef();
     const[name,setName]=useState("");
+    function setmusicname(name){
+        setName(name);
+    }
+    useEffect(()=>{
+        music.current.play();
+    },[name]);
     return(
         <React.Fragment>
             <Grid container className='justify-center'><Card item><CardContent><img src="./Tx/tt.jpg" className='max-w-4xl ' alt="" /></CardContent></Card> </Grid>
@@ -29,7 +36,7 @@ export const Piano=()=>{
                 <Grid item >
                     <Button variant='contained'className='h-64 w-20 bg-black' onClick={()=>setName("NI")}>NI</Button>
                 </Grid>
-            <Grid container><Card  item><CardContent><audio src={`Saregama/${name}.mp3`}></audio></CardContent></Card></Grid>
+            <Grid container><Card  item><CardContent><audio src={`../Saregama/${name}.mp3`} ref={music}></audio></CardContent></Card></Grid>
             </Grid>
         </React.Fragment>
     )
